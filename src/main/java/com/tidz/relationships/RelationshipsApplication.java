730557dfb48c8fb2842a72afc09e87591ac27732
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-26 14:51:48 -0300

    create a command line runner

--- src/main/java/com/tidz/relationships/RelationshipsApplication.java
+++ src/main/java/com/tidz/relationships/RelationshipsApplication.java
@@ -1,13 +1,22 @@
 package com.tidz.relationships;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RelationshipsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RelationshipsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLinerunner(String args[]) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
