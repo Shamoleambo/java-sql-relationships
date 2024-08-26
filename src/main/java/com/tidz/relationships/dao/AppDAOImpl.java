befc6b5e46465de91ce0b7f85b92553db21bfe15
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-26 15:17:49 -0300

    create the AppDAO implementation

---
+++ src/main/java/com/tidz/relationships/dao/AppDAOImpl.java
@@ -0,0 +1,25 @@
+package com.tidz.relationships.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tidz.relationships.entity.Instructor;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public AppDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public void save(Instructor instructor) {
+		this.entityManager.persist(instructor);
+	}
+
+}
