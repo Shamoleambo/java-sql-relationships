23155a71b2dd4153f17a1e4bc42f1f87697ed67d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-27 10:22:30 -0300

    add the field instructor to the InstructorDetail entity

--- src/main/java/com/tidz/relationships/entity/InstructorDetail.java
+++ src/main/java/com/tidz/relationships/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.tidz.relationships.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +21,9 @@ public class InstructorDetail {
 	private String youtubeChannel;
 	@Column(name = "hobby")
 	private String hobby;
+
+	@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	private Instructor instructor;
 
 	public InstructorDetail() {
 
@@ -51,6 +56,14 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	@Override
