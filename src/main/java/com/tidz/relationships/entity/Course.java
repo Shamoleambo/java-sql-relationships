32d3f9ca14d767e7b297ec56e64a9a75e59d4ece
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-28 09:36:10 -0300

    establish the relationship many to many for student and course

--- src/main/java/com/tidz/relationships/entity/Course.java
+++ src/main/java/com/tidz/relationships/entity/Course.java
@@ -11,6 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -35,12 +37,25 @@ public class Course {
 	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
 
+	@ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH }, fetch = FetchType.LAZY)
+	@JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+	private List<Student> students;
+
 	public Course() {
 
 	}
 
 	public Course(String title) {
 		this.title = title;
+	}
+
+	public void add(Student student) {
+		if (this.students == null) {
+			this.students = new ArrayList<>();
+		}
+
+		this.students.add(student);
 	}
 
 	public void add(Review review) {
@@ -81,6 +96,14 @@ public class Course {
 
 	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
 	}
 
 	@Override
--- src/main/java/com/tidz/relationships/entity/Student.java
+++ src/main/java/com/tidz/relationships/entity/Student.java
@@ -1,10 +1,18 @@
 package com.tidz.relationships.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +31,11 @@ public class Student {
 	@Column(name = "email")
 	private String email;
 
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH })
+	@JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+	private List<Course> courses;
+
 	public Student() {
 
 	}
@@ -31,6 +44,14 @@ public class Student {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+	}
+
+	public void add(Course course) {
+		if (this.courses == null) {
+			this.courses = new ArrayList<>();
+		}
+
+		this.courses.add(course);
 	}
 
 	public Long getId() {
@@ -63,6 +84,14 @@ public class Student {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
 	}
 
 	@Override
